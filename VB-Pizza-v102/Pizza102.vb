259ec73be489aa248583d7318b21eb97c8645a6e
Luca Gallelli <luca.gallelli@education.nsw.gov.au>
2021-11-17 12:23:05 +1100

    Add project files.

---
+++ VB-Pizza-v102/Pizza102.vb
@@ -0,0 +1,153 @@
+﻿Public Class Pizza102
+    'set up a record or "class" for a student
+    Class STUDENT
+        Public studID As Int16
+        Public firstName As String
+        Public lastName As String
+        Public deliveryDate As Date
+        Public postcode As String  'even though postcode is a number, we store it as a string
+        Public quantity As Byte    'Max quantity is 255 Pizzas
+        Public phoneNo As String   'Phone numbers are NEVER used to calculate, so saved as text
+        Public street As String
+        Public suburb As String
+        Public hour As Byte        '# between 0 and 23 - it can never be > 255
+        Public minute As Byte      '# between 0 and 59 - it can never be > 255
+        Public pizzaBase As String 'short name for base
+        Public top1 As Boolean
+        Public top2 As Boolean
+        Public top3 As Boolean
+        Public top4 As Boolean
+    End Class
+
+    Dim students(9) As STUDENT
+    Dim studentCount As Integer = 0
+    Dim basePrice As Single
+    Dim top1price As Single
+    Dim top2price As Single
+    Dim top3price As Single
+    Dim top4price As Single
+    Dim toppedPrice As Single
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'allocate memory
+        For i = 0 To 9
+            students(i) = New STUDENT
+        Next
+        'load default prices
+        basePrice = 10
+        top1price = 0.3
+        top2price = 0.45
+        top3price = 0.9
+        top4price = 1.3
+
+        'display prices
+        txtBasePrice.Text = FormatCurrency(basePrice)
+        txtTop1.Text = FormatCurrency(top1price)
+        txtTop2.Text = FormatCurrency(top2price)
+        txtTop3.Text = FormatCurrency(top3price)
+        txtTop4.Text = FormatCurrency(top4price)
+
+
+        'load 4 test records
+        students(0).studID = 1
+        students(0).firstName = "Johnny"
+        students(0).lastName = "Depp"
+        students(0).deliveryDate = "9/6/63"
+        students(0).postcode = "m"
+        students(0).quantity = 78.2
+        students(1).studID = 2
+        students(1).firstName = "Jennifer"
+        students(1).lastName = "Lawrence"
+        students(1).deliveryDate = "15/8/90"
+        students(1).postcode = "f"
+        students(1).quantity = 88.2
+        students(2).studID = 3
+        students(2).firstName = "George"
+        students(2).lastName = "Clooney"
+        students(2).deliveryDate = "6/5/61"
+        students(2).postcode = "m"
+        students(2).quantity = 68.2
+        students(3).studID = 4
+        students(3).firstName = "Scarlett"
+        students(3).lastName = "Johansson"
+        students(3).deliveryDate = "22/11/84"
+        students(3).postcode = "f"
+        students(3).quantity = 72.2
+        'set the student count to the number of students which have been entered
+        studentCount = 4
+        displayList()
+        CalcToppedPrice()
+        CalcTotalPrice()
+    End Sub
+    Private Sub btnAddStud_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        students(studentCount).studID = studentCount + 1 'allocate the new student ID to an incremented value
+        'place text from text boxes into the array - first students(0), then students(1), students(2) etc
+        students(studentCount).firstName = txtFirstName.Text
+        students(studentCount).lastName = txtLastName.Text
+        students(studentCount).deliveryDate = dteDelivery.Text & " " & cboHour.Text & ":" & cboMinute.Text
+        students(studentCount).postcode = txtPostcode.Text
+        students(studentCount).quantity = txtQuantity.Text
+        studentCount += 1
+        'return text boxes to blank ready for next entry
+        txtFirstName.Text = ""
+        txtLastName.Text = ""
+        dteDelivery.Text = ""
+        txtPostcode.Text = ""
+        txtQuantity.Text = 1
+        displayList()
+    End Sub
+    Private Sub displayList()
+        'clear the list box as it keeps the earlier loop
+        txtStList.Items.Clear()
+        'loop through the array to print all rows
+        For i = 0 To studentCount - 1
+            txtStList.Items.Add(students(i).studID & " - " & students(i).firstName & " - " &
+                              UCase(students(i).lastName) & " - " & students(i).deliveryDate & " - " &
+                              students(i).postcode & " - " & students(i).quantity)
+        Next
+    End Sub
+
+    Private Sub CalcToppedPrice()
+        toppedPrice = basePrice
+        If chkTop1.Checked Then 'later theese 4 IF statements could be replaced with a loop
+            toppedPrice += top1price
+        End If
+        If chkTop2.Checked Then
+            toppedPrice += top2price
+        End If
+        If chkTop3.Checked Then
+            toppedPrice += top3price
+        End If
+        If chkTop4.Checked Then
+            toppedPrice += top4price
+        End If
+        txtToppedPrice.Text = FormatCurrency(toppedPrice)
+    End Sub
+
+    Private Sub chkTop1_CheckedChanged(sender As Object, e As EventArgs) Handles chkTop1.CheckedChanged
+        CalcToppedPrice()
+        CalcTotalPrice()
+    End Sub
+
+    Private Sub chkTop2_CheckedChanged(sender As Object, e As EventArgs) Handles chkTop2.CheckedChanged
+        CalcToppedPrice()
+        CalcTotalPrice()
+    End Sub
+
+    Private Sub chkTop3_CheckedChanged(sender As Object, e As EventArgs) Handles chkTop3.CheckedChanged
+        CalcToppedPrice()
+        CalcTotalPrice()
+    End Sub
+
+    Private Sub chkTop4_CheckedChanged(sender As Object, e As EventArgs) Handles chkTop4.CheckedChanged
+        CalcToppedPrice()
+        CalcTotalPrice()
+    End Sub
+    Private Sub CalcTotalPrice()
+        txtTotalPrice.Text = FormatCurrency(toppedPrice * CInt(txtQuantity.Text))
+    End Sub
+
+    Private Sub txtQuantity_TextChanged(sender As Object, e As EventArgs) Handles txtQuantity.TextChanged
+        CalcTotalPrice()
+    End Sub
+End Class
---
+++ VB-StDB-v106/StDB106.Designer.vb
@@ -0,0 +1,239 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class StDB107
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtAvMk = New System.Windows.Forms.TextBox()
+        Me.btnAddStud = New System.Windows.Forms.Button()
+        Me.txtStList = New System.Windows.Forms.ListBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.chkPaid = New System.Windows.Forms.CheckBox()
+        Me.txtPhone = New System.Windows.Forms.MaskedTextBox()
+        Me.txtDOB = New System.Windows.Forms.DateTimePicker()
+        Me.txtGender = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(36, 83)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(100, 20)
+        Me.txtFirstName.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(36, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(107, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Enter Student Details"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(36, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "First Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(142, 64)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Last Name"
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(142, 83)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(100, 20)
+        Me.txtLastName.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(457, 64)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(66, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Date of Birth"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(245, 64)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(42, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Gender"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(351, 64)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(65, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Average Mk"
+        '
+        'txtAvMk
+        '
+        Me.txtAvMk.Location = New System.Drawing.Point(354, 83)
+        Me.txtAvMk.Name = "txtAvMk"
+        Me.txtAvMk.Size = New System.Drawing.Size(100, 20)
+        Me.txtAvMk.TabIndex = 4
+        '
+        'btnAddStud
+        '
+        Me.btnAddStud.Location = New System.Drawing.Point(39, 129)
+        Me.btnAddStud.Name = "btnAddStud"
+        Me.btnAddStud.Size = New System.Drawing.Size(104, 23)
+        Me.btnAddStud.TabIndex = 11
+        Me.btnAddStud.Text = "Add Student"
+        Me.btnAddStud.UseVisualStyleBackColor = True
+        '
+        'txtStList
+        '
+        Me.txtStList.FormattingEnabled = True
+        Me.txtStList.Location = New System.Drawing.Point(36, 202)
+        Me.txtStList.Name = "txtStList"
+        Me.txtStList.Size = New System.Drawing.Size(533, 160)
+        Me.txtStList.TabIndex = 15
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(39, 183)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(172, 13)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = "List of entered students (for testing)"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(251, 112)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(1, 0, 1, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(38, 13)
+        Me.Label7.TabIndex = 18
+        Me.Label7.Text = "Phone"
+        '
+        'chkPaid
+        '
+        Me.chkPaid.AutoSize = True
+        Me.chkPaid.Location = New System.Drawing.Point(372, 129)
+        Me.chkPaid.Margin = New System.Windows.Forms.Padding(1)
+        Me.chkPaid.Name = "chkPaid"
+        Me.chkPaid.Size = New System.Drawing.Size(47, 17)
+        Me.chkPaid.TabIndex = 6
+        Me.chkPaid.Text = "Paid"
+        Me.chkPaid.UseVisualStyleBackColor = True
+        '
+        'txtPhone
+        '
+        Me.txtPhone.Location = New System.Drawing.Point(248, 129)
+        Me.txtPhone.Mask = "0000-000-000"
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(100, 20)
+        Me.txtPhone.TabIndex = 5
+        '
+        'txtDOB
+        '
+        Me.txtDOB.Location = New System.Drawing.Point(460, 83)
+        Me.txtDOB.Name = "txtDOB"
+        Me.txtDOB.Size = New System.Drawing.Size(200, 20)
+        Me.txtDOB.TabIndex = 19
+        '
+        'txtGender
+        '
+        Me.txtGender.FormattingEnabled = True
+        Me.txtGender.Items.AddRange(New Object() {"Male", "Female ", "Other"})
+        Me.txtGender.Location = New System.Drawing.Point(248, 82)
+        Me.txtGender.MaxLength = 6
+        Me.txtGender.Name = "txtGender"
+        Me.txtGender.Size = New System.Drawing.Size(100, 21)
+        Me.txtGender.TabIndex = 20
+        '
+        'StDB107
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(674, 405)
+        Me.Controls.Add(Me.txtGender)
+        Me.Controls.Add(Me.txtDOB)
+        Me.Controls.Add(Me.txtPhone)
+        Me.Controls.Add(Me.chkPaid)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.txtStList)
+        Me.Controls.Add(Me.btnAddStud)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtAvMk)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Name = "StDB107"
+        Me.Text = "v1.06 Student Array of Records"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtAvMk As TextBox
+    Friend WithEvents btnAddStud As Button
+
+
+    Friend WithEvents txtStList As ListBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents chkPaid As CheckBox
+    Friend WithEvents txtPhone As MaskedTextBox
+    Friend WithEvents txtDOB As DateTimePicker
+    Friend WithEvents txtGender As ComboBox
+End Class
